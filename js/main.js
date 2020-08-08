d3f2b58286371dfadf6de768618fb0222ce82f6b
iteu <61872438+iteu@users.noreply.github.com>
2020-08-08 21:45:36 +0500

    TRAFICO

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+$('.feetback-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<img src="./img/left.png" alt=""></img>',
+    nextArrow: '<img src="./img/right.png" alt=""></img>',
+  });
